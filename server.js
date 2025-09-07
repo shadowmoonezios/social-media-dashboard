606912d630938032157b60e43fb984ca08b234b1
shadowmoonezios <agushifelde@hotmail.com>
2025-09-07 22:29:38 +0300

    Updated MongoDB connection string to use environment variable fallback correctly

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/social
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.error('Database connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the social media dashboard application');
