8823680375757b6f0ca3128d44edfe81a2074825
shadowmoonezios <agushifelde@hotmail.com>
2025-09-07 22:26:46 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Willkommen zum Social Media Dashboard</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
