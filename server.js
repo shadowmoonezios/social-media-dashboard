8ffeb8521b169edfb60e524fb8c7e6f70684c08e
shadowmoonezios <agushifelde@hotmail.com>
2025-09-07 22:27:31 +0300

    Fix typo in welcome message

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
   .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
-  res.send('Willkommen zum Social Media Dashboard');
+  res.send('Willkommen zum sozialen Medien Dashboard');
 });
 
 app.listen(PORT, () => {
